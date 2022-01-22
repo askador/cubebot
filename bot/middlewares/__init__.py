a5c9a0bd6914af3e92d95a2885133e8bbe9e6252
askador <fedosov0912@gmail.com>
2022-01-22 21:13:54 +0200

    commented logging middleware for god sake

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -11,6 +11,6 @@ from .i18n import i18nMiddleware
 
 def setup(dp: Dispatcher, config: Middlewares, pool: sessionmaker):
     dp.middleware.setup(ThrottlingMiddleware())
-    dp.middleware.setup(LoggingMiddleware())
+    # dp.middleware.setup(LoggingMiddleware())
     dp.middleware.setup(i18nMiddleware(config.i18n))
     dp.middleware.setup(DatabaseMiddleware(pool)) 
