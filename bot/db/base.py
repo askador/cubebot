9127360ebc09fad5a6f87a5646e6f10611623bb8
askador <fedosov0912@gmail.com>
2022-01-23 16:58:07 +0200

    bad env vars for db handling

--- bot/db/base.py
+++ bot/db/base.py
@@ -1,28 +1,42 @@
-import sys
 from loguru import logger
+from sys import exit
+from socket import gaierror
+from asyncpg.exceptions import InvalidPasswordError, InvalidCatalogNameError
 
+from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from bot.data.config import DB
+from bot.data.config import Database
 
 Base = declarative_base()
 
-async def create_pool(db: DB):
+async def create_pool(db: Database):
     logger.info("Establishing database pool")
 
     connection_uri = f"postgresql+asyncpg://{db.user}:{db.password}@{db.host}:{db.port}/{db.name}"
-    engine = create_async_engine(url=make_url(connection_uri))
-    pool: sessionmaker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False)
-
+    engine = create_async_engine(connection_uri, echo=False)
+    
     try:
-        async with pool() as pool:
-            await pool.execute("SELECT 1+1 AS RESULT") # type: ignore
-    except Exception as e:
+        async with engine.begin() as conn:
+            # initialize all tables
+            await conn.run_sync(Base.metadata.create_all)
+    except gaierror as addrexc:
+        logger.exception("host is invalid", addrexc)
+        exit()
+    except InvalidPasswordError as e:
         logger.exception(e)
-        sys.exit()
+        exit()
+    except InvalidCatalogNameError as e:
+        logger.exception(e)
+        exit()
+    except ConnectionRefusedError as e:
+        logger.exception("port is invalid", e)
+        exit()
+
+    pool: sessionmaker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False)
 
     return pool
 
@@ -32,5 +46,5 @@ Exceptions:
  password   - asyncpg.exceptions.InvalidPasswordError: password authentication failed for user "postgres"
  db_name    - asyncpg.exceptions.InvalidCatalogNameError: database "ubebot" does not exist
  host       - socket.gaierror: [Errno 11001] getaddrinfo failed
- port       - ectionRefusedError: [WinError 1225] The remote computer refused the network connection
+ port       - ConnectionRefusedError: [WinError 1225] The remote computer refused the network connection
 """
