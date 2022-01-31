4438166e701c22fc8f772fb1ab1331153a03f516
askador <fedosov0912@gmail.com>
2022-01-31 12:42:09 +0200

    remove load_config function

--- bot/data/config.py
+++ bot/data/config.py
@@ -8,6 +8,7 @@ from environs import Env
 class TgBot:
     token: str
     fsm_type: str
+    admins: list
 
 
 @dataclass
@@ -56,41 +57,42 @@ class Config:
     middlewares: Middlewares
 
 
-def load_config() -> Config:
-    env = Env()
-    env.read_env()
+env = Env()
+env.read_env()
+logs_path = Path.joinpath(Path(__file__).cwd(), f'logs_{env.str("BOT_USERNAME")}') if env.bool("SAVE_LOGS") else None
 
-    return Config(
-        bot=TgBot(
-            token=env.str("BOT_TOKEN"),
-            fsm_type=env.str("FSM_MODE")
-        ),
-        redis=Redis(
-            host=env.str("REDIS_HOST"),
-            port=env.int("REDIS_PORT"),
-            db=env.int("REDIS_DB_FSM"),
-            password=env.str("REDIS_PASSWORD")
-        ),
-        db=Database(
-            host=env.str("DB_HOST"),
-            port=env.int("DB_PORT"),
-            name=env.str("DB_NAME"),
-            user=env.str("DB_USER"),
-            password=env.str("DB_PASSWORD")
-        ),
-        logging=Logging(
-            path=Path.joinpath(Path(__file__).cwd(), f'logs_{env.str("BOT_USERNAME")}'),
-            level="INFO",
-            ignored=["aiogram.bot.api"]
-        ),
-        middlewares=Middlewares(
-            i18n=I18nConfig(
-                locales_path=Path.joinpath(
-                    Path(__file__).parent.parent, 'locales'),
-                default_language="ru",
-                allow_missing_translation=True,
-                allow_missing_placeholder=True,
-                allow_missing_plural=True
-            )
+config = Config(
+    bot=TgBot(
+        token=env.str("BOT_TOKEN"),
+        fsm_type=env.str("FSM_MODE"),
+        admins=env.list("BOT_ADMINS")
+    ),
+    redis=Redis(
+        host=env.str("REDIS_HOST"),
+        port=env.int("REDIS_PORT"),
+        db=env.int("REDIS_DB_FSM"),
+        password=env.str("REDIS_PASSWORD")
+    ),
+    db=Database(
+        host=env.str("DB_HOST"),
+        port=env.int("DB_PORT"),
+        name=env.str("DB_NAME"),
+        user=env.str("DB_USER"),
+        password=env.str("DB_PASSWORD")
+    ),
+    logging=Logging(
+        path=logs_path,
+        level=env.str("LOG_LEVEL"),
+        ignored=["aiogram.bot.api"]
+    ),
+    middlewares=Middlewares(
+        i18n=I18nConfig(
+            locales_path=Path.joinpath(
+                Path(__file__).parent.parent, 'locales'),
+            default_language="ru",
+            allow_missing_translation=True,
+            allow_missing_placeholder=True,
+            allow_missing_plural=True
         )
     )
+)
