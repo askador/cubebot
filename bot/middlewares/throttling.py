5511830c2aa747d1f9027f566f9c8ff13db029c4
askador <fedosov0912@gmail.com>
2022-02-09 16:31:18 +0200

    ttlcache based throttling

--- bot/middlewares/throttling.py
+++ bot/middlewares/throttling.py
@@ -4,13 +4,13 @@ from aiogram.dispatcher.handler import CancelHandler, current_handler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from cachetools import TTLCache
 
-THROTTLE_TIME_OTHER = 1    # время искусственной задержки между остальными командами, оно же период троттлинга
+THROTTLE_TIME_DEFAULT = 1    # время искусственной задержки между остальными командами, оно же период троттлинга
 THROTTLE_TIME_ROLL = 2     # время искусственной задержки между броском дайса и ответом, оно же период троттлинга
-THROTTLE_TIME_BET = 1      # время искусственной задержки между ставкой и ответом, оно же период троттлинга
+THROTTLE_TIME_BET = 0.8      # время искусственной задержки между ставкой и ответом, оно же период троттлинга
 
 # Разные по продолжительности кэши для разных типов действий (запуск игрового автомата или /-команды)
 caches = {
-    "default": TTLCache(maxsize=inf, ttl=THROTTLE_TIME_OTHER),
+    "default": TTLCache(maxsize=inf, ttl=THROTTLE_TIME_DEFAULT),
     "roll": TTLCache(maxsize=inf, ttl=THROTTLE_TIME_ROLL),
     "bet": TTLCache(maxsize=inf, ttl=THROTTLE_TIME_BET),
 }
@@ -20,7 +20,7 @@ caches = {
 class ThrottlingMiddleware(BaseMiddleware):
 
     def __init__(self):
-        super().__init__()
+        super(ThrottlingMiddleware, self).__init__()
 
     async def on_process_message(self, message: types.Message, data: dict):
         user_id = message.from_user.id
