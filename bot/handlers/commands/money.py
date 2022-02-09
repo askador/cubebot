8148c7fc48832cf3df8e11a1b7ab0be4e9c8ace3
askador <fedosov0912@gmail.com>
2022-02-09 16:25:07 +0200

    player money

---
+++ bot/handlers/commands/money.py
@@ -0,0 +1,17 @@
+from aiogram import Dispatcher, types
+from aiogram.dispatcher.filters import Command
+
+from bot.db.models import Player
+from bot.filters import StrictCommand
+from bot.types.Localization import I18nJSON
+from bot.utils.throttling import rate_limit
+
+
+@rate_limit()
+async def money(message: types.Message, player: Player) -> None:
+    await message.reply(f"{player.money:,}")
+
+
+def register(dp: Dispatcher):
+    dp.register_message_handler(money, Command('money'), StrictCommand())
+    dp.register_message_handler(money, Command('деньги', prefixes='!'))
