52165292117b696db370184196c8bd847ed69f4e
askador <fedosov0912@gmail.com>
2022-02-09 16:33:37 +0200

    bot ui commands

--- bot/utils/set_bot_commands.py
+++ bot/utils/set_bot_commands.py
@@ -1,23 +1,76 @@
-from aiogram.types import BotCommand, BotCommandScopeDefault
+from aiogram.types import BotCommand, BotCommandScopeDefault, BotCommandScopeAllPrivateChats
 from aiogram import Bot
 
 
-async def set_bot_commands(bot: Bot):
-    data = [
-        (
-            [
-                BotCommand("start", "Приветственное сообщение"),
-                BotCommand("help", "Помощь"),
-                BotCommand("game", "Начать новую игру"),
-                BotCommand("roll", "Бросить кости"),
-                BotCommand("money", "Просмотреть баланс"),
-                BotCommand("profile", "Профиль игрока"),
-                BotCommand("issue", "Сообщить о проблеме с ботом"),
-            ],
-            BotCommandScopeDefault(),
-            None
-        )
-    ]
-    for commands_list, commands_scope, language in data:
-        await bot.set_my_commands(commands=commands_list, scope=commands_scope, language_code=language)
+DEFAULT_SCOPE_COMMANDS = (
+    [
+        BotCommand("start", "Приветственное сообщение"),
+        BotCommand("help", "Помощь"),
+        BotCommand("game", "Начать новую игру"),
+        BotCommand("roll", "Бросить кости"),
+        BotCommand("logs", "Логи 10 предыдущих игр"),
+        BotCommand("rating", "Рейтинг по чату"),
+        BotCommand("money", "Просмотреть баланс"),
+        BotCommand("profile", "Профиль игрока"),
+    ],
+    BotCommandScopeDefault(),
+    None
+)
 
+EN_SCOPE_COMMANDS = (
+    [
+        BotCommand("start", "Greeting message"),
+        BotCommand("help", "Help message"),
+        BotCommand("game", "Start a new game"),
+        BotCommand("roll", "Roll the dice"),
+        BotCommand("logs", "Logs of 10 previous games"),
+        BotCommand("rating", "Rating"),
+        BotCommand("money", "Current player money"),
+        BotCommand("profile", "Player Profile"),
+    ],
+    BotCommandScopeDefault(),
+    'en'
+)
+
+PRIVATE_CHAT_DEFAULT_SCOPE_COMMANDS = (
+    [
+        BotCommand("start", "Приветственное сообщение"),
+        BotCommand("help", "Помощь"),
+        BotCommand("game", "Начать новую игру"),
+        BotCommand("roll", "Бросить кости"),
+        BotCommand("logs", "Логи 10 предыдущих игр"),
+        BotCommand("money", "Просмотреть баланс"),
+        BotCommand("profile", "Профиль игрока"),
+        BotCommand("issue", "Сообщить о проблеме с ботом"),
+    ],
+    BotCommandScopeAllPrivateChats(),
+    None
+)
+
+PRIVATE_CHAT_EN_SCOPE_COMMANDS = (
+    [
+        BotCommand("start", "Greeting message"),
+        BotCommand("help", "Help message"),
+        BotCommand("game", "Start a new game"),
+        BotCommand("roll", "Roll the dice"),
+        BotCommand("logs", "Logs of 10 previous games"),
+        BotCommand("money", "Current player money"),
+        BotCommand("profile", "Player Profile"),
+        BotCommand("issue", "Report an issue with bot"),
+    ],
+    BotCommandScopeAllPrivateChats(),
+    'en'
+)
+
+
+async def set_bot_commands(bot: Bot):
+
+    commands = [
+        DEFAULT_SCOPE_COMMANDS,
+        EN_SCOPE_COMMANDS,
+        PRIVATE_CHAT_DEFAULT_SCOPE_COMMANDS,
+        PRIVATE_CHAT_EN_SCOPE_COMMANDS
+    ]
+
+    for commands_list, commands_scope, language in commands:
+        await bot.set_my_commands(commands=commands_list, scope=commands_scope, language_code=language)
