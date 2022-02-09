ad424540e5061d4cd5e1e93d159d740f23d04e40
askador <fedosov0912@gmail.com>
2022-02-09 16:32:41 +0200

    setup middlewares

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,16 +1,21 @@
 from aiogram import Dispatcher
 from sqlalchemy.orm import sessionmaker
 
-from bot.data.config import Middlewares
+from bot.data.config import I18nConfig, Middlewares
 
+from .acl import ACLMiddleware
 from .database import DatabaseMiddleware
 from .throttling import ThrottlingMiddleware
 from .logging import LoggingMiddleware
 from .i18n import i18nMiddleware
+from .only_admins import OnlyAdmins
 
 
-def setup(dp: Dispatcher, config: Middlewares, pool: sessionmaker):
-    dp.middleware.setup(ThrottlingMiddleware())
-    # dp.middleware.setup(LoggingMiddleware())
-    dp.middleware.setup(i18nMiddleware(config.i18n))
-    dp.middleware.setup(DatabaseMiddleware(pool)) 
+def setup(dp: Dispatcher, i18n: I18nConfig, pool: sessionmaker, is_dev: bool):
+    if is_dev:
+        dp.middleware.setup(OnlyAdmins())           # pre_process
+    # dp.middleware.setup(LoggingMiddleware())  
+    dp.middleware.setup(DatabaseMiddleware(pool))   # pre_process
+    dp.middleware.setup(ThrottlingMiddleware())     # process
+    dp.middleware.setup(ACLMiddleware())            # process
+    dp.middleware.setup(i18nMiddleware(i18n))       # process
