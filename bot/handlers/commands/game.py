fc50129d30000e64e799edf0952c57b450f97086
askador <fedosov0912@gmail.com>
2022-02-09 16:25:35 +0200

    create new game

---
+++ bot/handlers/commands/game.py
@@ -0,0 +1,35 @@
+from contextlib import suppress
+
+from aiogram import Dispatcher, types
+from aiogram.dispatcher.filters import Command
+from aiogram.utils.exceptions import ChatNotFound
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from bot.db.models import Game
+from bot.filters import StrictCommand
+from bot.types.Localization import I18nJSON
+from bot.keyboards.inline import game_kb
+from bot.utils.throttling import rate_limit
+
+@rate_limit()
+async def new_game(message: types.Message, i18n: I18nJSON, session: AsyncSession):
+    chat_id = message.chat.id
+
+    game = (await session.execute(
+        select(Game)
+        .where(Game.chat_id == chat_id)
+    )).fetchone()
+    
+    if game:
+        return await message.answer(i18n.t('commands.game.already_exists'))
+
+    session.add(Game(chat_id=chat_id))
+    await session.commit()
+
+    await message.answer(i18n.t('commands.game.message'), reply_markup=game_kb())
+
+
+def register(dp: Dispatcher):
+    dp.register_message_handler(new_game, Command('game'), StrictCommand())
