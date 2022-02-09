4bef3a940343dc42692ae7a15e2e06e1a881365e
askador <fedosov0912@gmail.com>
2022-02-09 16:24:07 +0200

    bind filters

--- bot/filters/__init__.py
+++ bot/filters/__init__.py
@@ -1,7 +1,12 @@
 from aiogram import Dispatcher
 
-from .is_admin import AdminFilter
-
+from .is_admin import IsBotAdmin
+from .is_bet import IsBet
+from .strict_command import StrictCommand
+from .can_roll import CanRoll
 
 def setup(dp: Dispatcher):
-    dp.bind_filter(AdminFilter)
+    dp.bind_filter(IsBotAdmin)
+    dp.bind_filter(IsBet, event_handlers=[dp.message_handlers])
+    dp.bind_filter(StrictCommand, event_handlers=[dp.message_handlers])
+    dp.bind_filter(CanRoll, event_handlers=[dp.message_handlers])
