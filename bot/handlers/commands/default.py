2e4707a26b6588e0f7b764cb44b049868ad7e0a1
askador <fedosov0912@gmail.com>
2022-02-09 16:25:50 +0200

    default commands

---
+++ bot/handlers/commands/default.py
@@ -0,0 +1,20 @@
+from aiogram import Dispatcher, types
+from aiogram.dispatcher.filters import CommandStart, CommandHelp, Command
+
+from bot.filters import StrictCommand
+from bot.types.Localization import I18nJSON
+from bot.utils.throttling import rate_limit
+
+
+@rate_limit()
+async def start(message: types.Message, i18n: I18nJSON):
+    await message.answer(i18n.t('commands.default.start'))
+
+@rate_limit()
+async def help(message: types.Message, i18n: I18nJSON):
+    await message.answer(i18n.t('commands.default.help'))
+
+
+def register(dp: Dispatcher):
+    dp.register_message_handler(start, CommandStart(), StrictCommand())
+    dp.register_message_handler(help, CommandHelp(), StrictCommand())
