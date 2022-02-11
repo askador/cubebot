e8dc2a8c5e228d42b20d5ff980bda006b39991d9
askador <fedosov0912@gmail.com>
2022-02-12 01:49:14 +0200

    add imports

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,3 +1,5 @@
 from . import logging
 from .throttling import rate_limit
 from .set_bot_commands import set_bot_commands
+from .redis_storage import redis_storage
+from .get_allowed_updates_list import get_allowed_updates_list
