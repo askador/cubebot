3ecacc8a4ad2f0f97a0d925938b9b035cd6df9ff
askador <fedosov0912@gmail.com>
2022-02-12 01:51:18 +0200

    TODO: use locales text from locales folder

--- bot/keyboards/inline/new_game.py
+++ bot/keyboards/inline/new_game.py
@@ -1,19 +1,45 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
 
 
-def new_game_kb() -> InlineKeyboardMarkup:
+def new_game_kb(locale: str = 'ru') -> InlineKeyboardMarkup:
+    locales = {
+        "ru": "{amount} на {number}",
+        "en": "{amount} on {number}"
+    }
+
+    cd = CallbackData('game', 'action', 'amount', 'number')
+    line = locales.get(locale, "{amount} на {number}")
+
     kb = [
         [
-            InlineKeyboardButton("100 на 1", callback_data="ставка 100-1"),
-            InlineKeyboardButton("100 на 2", callback_data="ставка 100-2"),
-            InlineKeyboardButton("100 на 3", callback_data="ставка 100-3"),
+            InlineKeyboardButton(
+                line.format(amount=100, number=1),
+                callback_data=cd.new(action='bet', amount=100, number='1')
+            ),
+            InlineKeyboardButton(
+                line.format(amount=100, number=2),
+                callback_data=cd.new(action='bet', amount=100, number='2')
+            ),
+            InlineKeyboardButton(
+                line.format(amount=100, number=3),
+                callback_data=cd.new(action='bet', amount=100, number='3')
+            ),
         ],
         [
-            InlineKeyboardButton("100 на 4", callback_data="ставка 100-4"),
-            InlineKeyboardButton("100 на 5", callback_data="ставка 100-5"),
-            InlineKeyboardButton("100 на 6", callback_data="ставка 100-6"),
+            InlineKeyboardButton(
+                line.format(amount=100, number=4),
+                callback_data=cd.new(action='bet', amount=100, number='4')
+            ),
+            InlineKeyboardButton(
+                line.format(amount=100, number=5),
+                callback_data=cd.new(action='bet', amount=100, number='5')
+            ),
+            InlineKeyboardButton(
+                line.format(amount=100, number=6),
+                callback_data=cd.new(action='bet', amount=100, number='6')
+            ),
         ],
     ]
-
 
     return InlineKeyboardMarkup(inline_keyboard=kb)
