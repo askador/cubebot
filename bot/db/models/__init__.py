e055e00f176dca0cebec16f3dbe5f2d3a4c4a0e1
askador <fedosov0912@gmail.com>
2022-02-12 01:44:40 +0200

    bot db models

---
+++ bot/db/models/__init__.py
@@ -0,0 +1,8 @@
+from bot.db.base import Base
+
+from .Player import Player
+from .ChatLog import ChatLog
+from .Bet import Bet
+from .Game import Game
+from .Chat import Chat
+from .Issue import Issue
