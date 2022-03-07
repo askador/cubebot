ba6bf92d86ea10f841619954660148dea3c953a4
askador <fedosov0912@gmail.com>
2022-03-07 23:11:39 +0200

    register all middlewares

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,7 +1,7 @@
 from aiogram import Dispatcher
 from sqlalchemy.orm import sessionmaker
 
-from bot.data.config import I18nConfig, Middlewares
+from bot.data.config import I18nConfig
 
 from .acl import ACLMiddleware
 from .database import DatabaseMiddleware
@@ -11,11 +11,11 @@ from .i18n import i18nMiddleware
 from .only_admins import OnlyAdmins
 
 
-def setup(dp: Dispatcher, i18n: I18nConfig, pool: sessionmaker, is_dev: bool):
+def setup(dp: Dispatcher, i18n: I18nConfig, pool: sessionmaker, is_dev: bool = False):
     if is_dev:
         dp.middleware.setup(OnlyAdmins())           # pre_process
     dp.middleware.setup(DatabaseMiddleware(pool))   # pre_process
-    # dp.middleware.setup(LoggingMiddleware())  
+    dp.middleware.setup(ACLMiddleware())            # pre_process
+    dp.middleware.setup(LoggingMiddleware())        
     dp.middleware.setup(ThrottlingMiddleware())     # process
-    dp.middleware.setup(ACLMiddleware())            # process
     dp.middleware.setup(i18nMiddleware(i18n))       # process
