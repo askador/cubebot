bbdd66a644aa2ad5fa0aa91bfda3091c3c7addba
askador <fedosov0912@gmail.com>
2022-03-11 00:47:53 +0200

    split can_roll into game and has_bets filters

--- bot/filters/__init__.py
+++ bot/filters/__init__.py
@@ -3,10 +3,12 @@ from aiogram import Dispatcher
 from .is_admin import IsBotAdmin
 from .is_bet import IsBet
 from .strict_command import StrictCommand
-from .can_roll import CanRoll
+from .player_has_bets import PlayerHasBets
+from .game_is_active import GameIsActive
 
 def setup(dp: Dispatcher):
     dp.bind_filter(IsBotAdmin)
     dp.bind_filter(IsBet, event_handlers=[dp.message_handlers])
     dp.bind_filter(StrictCommand, event_handlers=[dp.message_handlers])
-    dp.bind_filter(CanRoll, event_handlers=[dp.message_handlers])
+    dp.bind_filter(PlayerHasBets, event_handlers=[dp.message_handlers, dp.callback_query_handlers])
+    dp.bind_filter(GameIsActive, event_handlers=[dp.message_handlers, dp.callback_query_handlers])
