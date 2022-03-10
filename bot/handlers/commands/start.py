9196bc368e3ee8cdfe3e914977f0e689d78f1edb
askador <fedosov0912@gmail.com>
2022-03-11 00:51:34 +0200

    split default into start and help commands files

---
+++ bot/handlers/commands/start.py
@@ -0,0 +1,19 @@
+from aiogram import Dispatcher, types
+from aiogram.dispatcher.filters import CommandStart, CommandHelp
+
+from bot.filters import StrictCommand
+from bot.types.Localization import I18nJSON
+from bot.utils.throttling import rate_limit
+from bot.analytics import analytics
+from bot.analytics.events import EventAction, EventCommand
+
+
+@rate_limit()
+@analytics.command(EventCommand.START)
+async def start(message: types.Message, i18n: I18nJSON):
+    await message.answer(i18n.t('commands.start'))
+    await analytics.action(message.chat.id, EventAction.SEND_MESSAGE)
+
+
+def register(dp: Dispatcher):
+    dp.register_message_handler(start, CommandStart(), StrictCommand())
