0dd28c8b74931a89cd323f79390748b818c6fb07
askador <fedosov0912@gmail.com>
2022-03-11 00:48:21 +0200

    multibet messages

--- bot/filters/is_bet.py
+++ bot/filters/is_bet.py
@@ -1,28 +1,38 @@
 import re
-from typing import Union
+from typing import Callable, Union
 
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
+from aiogram.dispatcher.handler import ctx_data
 
 
 class IsBet(BoundFilter):
-    async def check(self, message: types.Message) -> Union[bool, "dict[str, dict]"]:
+    async def check(self, message: types.Message) -> Union[bool, dict[str, list[tuple[int, str]]]]:
+        match: Callable[[str], Union[re.Match, None]] = lambda text: re.match(r'^([0-9_,]+) (на )?([1-6]|([1-6]-[1-6]))$', text)
+
         # remove command at the beginning `!ставка 100 1`
-        text = message.text.replace("!ставка ", '').replace("!с ", '')
+        text = message.text.replace("!ставка ", '').replace("!с ", '').split('\n')
 
-        if re.match(r'^([0-9_,]+) (на )?([1-6]|([1-6]-[1-6]))$', text) is None:
+        if len(text) > 10:
             return False
+
+        if not all([match(bet) for bet in text]):
+            return False
+
+        bets = []
+        for bet in text:
+            amount, numbers = bet.split()
+            amount = amount.replace('_', '').replace(',', '')
+            if amount == '':    # check if bet is not like '__ 1' 
+                return False
             
-        amount = text.split()[0].replace('_', '').replace(',', '')
-        numbers = text.split()[-1]
-        
-        if amount == '': # check if bet is not like '__ 1' 
-            return False
+            if int(amount) == 0:
+                return False
 
-        if int(amount) == 0:
-            return False
+            if len(numbers) > 1 and numbers[2] <= numbers[0]:
+                return False 
 
-        if len(numbers) > 1 and numbers[2] <= numbers[0]:
-            return False 
+            bets.append((amount, numbers))
 
-        return {"bet_data": {"amount": int(amount), "numbers": numbers}}
+
+        return {"bets": bets}
