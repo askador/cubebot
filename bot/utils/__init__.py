79e1b30dd58a83f9661596527cd175f42db112f8
askador <fedosov0912@gmail.com>
2022-03-11 15:14:36 +0200

    on_shutdown function

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -3,3 +3,4 @@ from .throttling import rate_limit
 from .set_bot_commands import set_bot_commands
 from .redis_storage import redis_storage
 from .get_allowed_updates_list import get_allowed_updates_list
+from .on_shutdown import on_shutdown
