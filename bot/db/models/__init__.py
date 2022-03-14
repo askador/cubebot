9e890a5e19644ee139629106d970be3fcfa3d2af
askador <fedosov0912@gmail.com>
2022-03-14 17:53:34 +0200

    bad commit

--- bot/db/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from bot.db.base import Base
-
-from .Player import Player
-from .ChatLog import ChatLog
-from .Bet import Bet
-from .Game import Game
-from .Chat import Chat
-from .Issue import Issue
